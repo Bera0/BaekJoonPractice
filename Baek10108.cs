eb03e0beb9e26b3a78219741c4052b34a64828e5
Bera0 <88365985+Bera0@users.noreply.github.com>
2022-09-24 17:30:47 +0900

    #1_10108

---
+++ Baek10108.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(int.Parse(Console.ReadLine()) - 543);
+        }
+    }
+} 
