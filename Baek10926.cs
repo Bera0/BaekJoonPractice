d64da5c5cdec847ffc7e98acd375bd166be59948
Bera0 <88365985+Bera0@users.noreply.github.com>
2022-09-24 17:23:51 +0900

    BaekJoon C# 10926
    C# 10926

---
+++ Baek10926.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpPrac
+{
+    class Baek10926
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Console.ReadLine() + "??!");
+        }
+    }
+}
